0faf60fdb05f3314d4af2907044df8f3d1bf36ca
alecfraserics3u <83348717+alecfraserics3u@users.noreply.github.com>
2021-05-28 23:56:54 -0400

    Update calculator.js

--- unit2/calculator.js
+++ unit2/calculator.js
@@ -1,23 +1,20 @@
-nombre1 = parseInt(prompt("Quel est votre premier nombre? "))
-operation = parseInt(prompt("Quel est l'operation? (+,-,*,/,% (1-5) "))
-nombre2 = parseInt(prompt("Quel est votre deuxieme nombre? "))
-document.write("REPONSE: ")
-switch (true){ // switch statement
- case operation==1:
-   document.write(nombre1 + nombre2) 
-   break;
- case operation==2:
-   document.write(nombre1 - nombre2)
-   break;
- case operation ==3:
-   document.write(nombre1 * nombre2)
-   break;
- case operation ==4:
-   document.write(nombre1 / nombre2)
-   break;
- case operation ==5:
-   document.write(nombre1 % nombre2)
-   break;
- case operation ==6:
-   document.write("Erreur")
-}
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Afficheur de nom, 2.6</title>
+  </head>
+  <body>
+    <h2>Afficheur de nom, par Alec Fraser</h2>
+    <form>
+      Quel est votre prenom? <input type="text" id="prenom"><br>
+      Quel est votre nom du millieu? <input type="text" id="millieu"><br>
+      Quel est votre dernier nom? <input type="text" id="famille"><br><br>
+      <input type="button" onclick="getInitials()" value="Afficher les initiales" style="background-color:red; border-color:blue; color:white">
+      <input type="button" onclick="getFull()" value="Afficher le nom complet" style="background-color:red; border-color:blue; color:white">
+      <input type="button" onclick="getUser()" value="Afficher le nom d'utulisateur" style="background-color:red; border-color:blue; color:white">
+    </form>
+    <p id="print"></p>
+    <script src="formulaire.js"></script>
+  </body>
+</html>
